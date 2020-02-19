e5d8260401fa525558b4a8e7c9e7aac3f0c400d2
Jefferson <jefferson@riverstonetech.com>
2020-02-19 19:17:17 +0530

    Initial commit

---
+++ test/test_helper.rb
@@ -0,0 +1,16 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+ENV['RAILS_ENV'] ||= 'test'
+>>>>>>> 9081f2e25557621276dfae85d109995c38fb2afd
+>>>>>>> b96fa26bcabff0207031c5fbd1e65538220223b1
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
